3885256f12b530e20370f4e5b354305af22e6e54
S-Patel05 <sohampatel8609@gmail.com>
2026-05-22 21:04:37 +0530

    Footer , layout neighbar , pageloader implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css'
+
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import PageLoader from "./components/PageLoader";
+import { useAuth } from '@clerk/react';
+import Layout from './components/Layout';
 
 function App() {
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) return <PageLoader />;
    return (
-    <>
+      <Layout>
       <header>
         <Show when="signed-out">
           <SignInButton mode='modal'/>
@@ -12,9 +18,13 @@ function App() {
         <Show when="signed-in">
           <UserButton />
         </Show>
+          <h1 className="text-red-500 text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button className='btn btn-primary'>click me</button>
       </header>
-    </>
-  )
+  </Layout>
+  )  
 }
 
 export default App
