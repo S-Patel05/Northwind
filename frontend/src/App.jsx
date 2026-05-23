5a9610210930dee0bbfb60f6d67c183a7b314ee2
S-Patel05 <sohampatel8609@gmail.com>
2026-05-23 21:04:50 +0530

    order page impliment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import PageLoader from "./components/PageLoader";
 import { useAuth } from '@clerk/react';
 import Layout from './components/Layout';
 import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
+import OrdersPage from "./pages/OrdersPage";
 import { Routes, Route, Navigate } from "react-router";
 
 function App() {
@@ -14,6 +16,11 @@ function App() {
  <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+                <Route
+          path="/orders"
+          element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} replace />}
+        />
       </Routes>
     </Layout>
   )  
