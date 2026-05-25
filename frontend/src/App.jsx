391e7be87949faf8a282a9e1c47616257d0fa0f7
S-Patel05 <sohampatel8609@gmail.com>
2026-05-25 18:08:58 +0530

    impliment admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import OrderDetailPage from "./pages/OrderDetailPage";
 import OrderSummaryPage from "./pages/OrderSummaryPage";
 import OrderChatPage from "./pages/OrderChatPage";
 import OrderVideoPage from "./pages/OrderVideoPage";
+import AdminProductsPage from "./pages/AdminProductsPage";
 import { Routes, Route, Navigate } from "react-router";
 
 function App() {
@@ -32,6 +33,11 @@ function App() {
         <Route
           path="/orders/:id/call"
           element={isSignedIn ? <OrderVideoPage /> : <Navigate to={"/"} replace />}
+        />
+
+        <Route
+          path="/admin"
+          element={isSignedIn ? <AdminProductsPage /> : <Navigate to="/" replace />}
         />
 
          <Route path="/orders/:id" element={<OrderDetailPage />}>
